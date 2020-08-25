c0f7d32ba367eeb4791c6ec6a9af7c61c58edfeb
Bibek Saini <itsbibeksaini@gmail.com>
2020-08-25 14:26:36 +0530

    Content list section

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,11 @@ import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarOptionSelectorDirective } from './directives/sidebar-option-selector/sidebar-option-selector.directive';
+import { ListComponent } from './list/list.component';
 
 
 @NgModule({
-  declarations: [MainLayoutComponent, SideBarComponent, FooterComponent, SidebarOptionSelectorDirective],
+  declarations: [MainLayoutComponent, SideBarComponent, FooterComponent, SidebarOptionSelectorDirective, ListComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
