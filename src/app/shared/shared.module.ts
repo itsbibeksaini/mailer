a70495bc07ad6050af1b0a63f98bf0126fc51a4f
Bibek Saini <itsbibeksaini@gmail.com>
2020-08-25 11:42:25 +0530

    Adding main layout side bar and footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MainLayoutComponent, SideBarComponent, FooterComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
