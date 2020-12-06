4c219be539b7dd8f16eedb0f5662c575ea6b77e8
Bibek Saini <itsbibeksaini@gmail.com>
2020-12-06 16:19:40 +0530

    Removing old mailer app

--- src/app/shared/directives/read-mail/read-mail.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReadMailDirective } from './read-mail.directive';
-
-describe('ReadMailDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ReadMailDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/shared/directives/sidebar-option-selector/sidebar-option-selector.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SidebarOptionSelectorDirective } from './sidebar-option-selector.directive';
-
-describe('SidebarOptionSelectorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SidebarOptionSelectorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
