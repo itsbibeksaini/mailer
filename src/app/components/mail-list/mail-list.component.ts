1a67aed3d57e04501a773a50ef310d64923cd3f3
Bibek Saini <itsbibeksaini@gmail.com>
2020-12-13 00:20:33 +0530

    UI Updates

--- src/app/components/mail-list/mail-list.component.ts
+++ src/app/components/mail-list/mail-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Mail } from 'src/app/core/models';
+import { MailStoreService } from 'src/app/core/store/mail-store/mail-store.service';
+import {filter, switchMap, take} from 'rxjs/operators'
+import { MailService } from 'src/app/core/fake-backend/mail/mail.service';
+import { MailQueryService } from 'src/app/core/query/mail-query/mail-query.service';
 
 @Component({
   selector: 'mail-list',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MailListComponent implements OnInit {
 
-  constructor() { }
+  loading:boolean = false
+  mails:Mail[] = []
+
+  constructor(private mailQuery: MailQueryService, 
+              private mailStore: MailStoreService,
+              private mailService: MailService) { }
 
   ngOnInit(): void {
+    this.mailQuery.getIsLoading().subscribe(res => this.loading = res)
+    this.mailQuery.getMails().subscribe(res => this.mails = res)
+
+    // load this from api
+    this.mailQuery.getLoaded().pipe(
+      take(1),
+      filter(res => !res),
+      switchMap(() =>{
+        this.mailStore.setLoading(true)
+        return this.mailService.getMails() // get data from backend api or fake backend 
+      }) 
+    ).subscribe(res => {
+      this.mailStore.update(state => {
+        return {
+          mails: res
+        }
+      })
+      this.mailStore.setLoading(false)
+    }, err =>{
+      console.log(err)
+      this.mailStore.setLoading(false)
+    })
   }
 
 }
