7137fecd23e086d33a66aa09321f079e00ba4753
Bibek Saini <itsbibeksaini@gmail.com>
2020-12-13 14:28:37 +0530

    Side bar bubble counter

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, OnInit, Renderer2, ViewChild, ViewContainerRef } from '@angular/core';
 import { faChevronLeft, faChevronRight, faEdit, faFile, faHome, faInbox, faPaperPlane, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { MailQueryService } from 'src/app/core/query/mail-query/mail-query.service';
+import { MailStoreService } from 'src/app/core/store/mail-store/mail-store.service';
 
 @Component({
   selector: 'sidebar',
@@ -16,10 +18,13 @@ export class SidebarComponent implements OnInit {
   expandColapseIcon = faChevronRight
 
   
-  
-  constructor(private rendrer:Renderer2, private sideBar: ElementRef) { }
+  inboxCounter = 0
+  constructor(private mailQuery: MailQueryService, private mailStore: MailStoreService) { }
 
   ngOnInit(): void {
+    this.mailQuery.getMails().subscribe(res =>{
+      this.inboxCounter = res.length
+    })
   }
 
 }
